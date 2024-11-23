63f2425f3217ecc0463e88a781d083c1089abbd9
samnumber7 <samnumber7@gmail.com>
2024-11-23 03:09:53 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Debounced-Slider
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/samnumber7/Debounced-Slider)
---
+++ src/example/slider-configurable-example.ts
@@ -0,0 +1,38 @@
+import {Component} from '@angular/core';
+import {MatSliderModule} from '@angular/material/slider';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {FormsModule} from '@angular/forms';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatCardModule} from '@angular/material/card';
+
+/**
+ * @title Configurable slider
+ */
+@Component({
+  selector: 'slider-configurable-example',
+  templateUrl: 'slider-configurable-example.html',
+  styleUrl: 'slider-configurable-example.css',
+  imports: [
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    MatCheckboxModule,
+    MatSliderModule,
+  ],
+})
+export class SliderConfigurableExample {
+  disabled = false;
+  max = 100;
+  min = 0;
+  showTicks = false;
+  step = 1;
+  thumbLabel = false;
+  value = 0;
+}
+
+
+/**  Copyright 2024 Google LLC. All Rights Reserved.
+    Use of this source code is governed by an MIT-style license that
+    can be found in the LICENSE file at https://angular.io/license */
